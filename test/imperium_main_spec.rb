60aaafcf05b22442164a2e35e6bf4b2afe603c42
Maksim Sustretov <waynesayonara@users.noreply.github.com>
2014-08-18 20:01:42 +0400

    Fail test to check whether notification works. take 2. #1

--- test/imperium_main_spec.rb
+++ test/imperium_main_spec.rb
@@ -6,6 +6,6 @@ require_relative 'spec_helper'
 describe Imperium::ImperiumMain do
   it 'has version number in its caption' do
     instance = Imperium::ImperiumMain.new;
-    expect(instance.caption).to include("some text #{Imperium::VERSION}")
+    expect(instance.caption).to include("some text. take 2 #{Imperium::VERSION}")
   end
 end
