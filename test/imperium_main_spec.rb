fe57a75504374cf7550197fba16eb284cfc3bcb6
d3lavar <d3lavar@gmail.com>
2014-08-18 21:54:29 +0400

    Test fail test

--- test/imperium_main_spec.rb
+++ test/imperium_main_spec.rb
@@ -7,5 +7,6 @@ describe Imperium::ImperiumMain do
   it 'has version number in its caption' do
     instance = Imperium::ImperiumMain.new;
     expect(instance.caption).to include("#{Imperium::VERSION}")
+    assert true = false
   end
 end
