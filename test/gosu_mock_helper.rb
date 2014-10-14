982e3650c38f4689335994dea5f483607164bb1f
Maksim Sustretov <waynesayonara@users.noreply.github.com>
2014-10-14 23:42:17 +0400

    failed map spec - reverted gosu_mock_helper usage #4

--- test/gosu_mock_helper.rb
+++ test/gosu_mock_helper.rb
@@ -1,31 +1,37 @@
 require_relative 'spec_helper'
 
+$window_back_up
 $window = Imperium::ImperiumMain.new
+
 RSpec.configure do |config|
-  config.before(:all) do
-    GosuWindowBackup ||= Gosu::Window
-    GosuImageBackup ||= Gosu::Image
-    Gosu.send(:remove_const, :Window)
-    Gosu.send(:remove_const, :Image)
-    Gosu::Window = GosuMock::Window
-    Gosu::Image = GosuMock::Image
+  # config.before(:all) do
+  #   GosuWindowBackup ||= Gosu::Window
+  #   GosuImageBackup ||= Gosu::Image
+  #   Gosu.send(:remove_const, :Window)
+  #   Gosu.send(:remove_const, :Image)
+  #   Gosu::Window = GosuMock::Window
+  #   Gosu::Image = GosuMock::Image
+  #
+  #   $window_back_up = $window
+  #   $window = GosuMock::Window.new(200, 200, false)
+  # end
+  #
+  # config.after(:all) do
+  #   Gosu.send(:remove_const, :Window)
+  #   Gosu.send(:remove_const, :Image)
+  #   Gosu::Window = GosuWindowBackup
+  #   Gosu::Image = GosuImageBackup
+  #
+  #   $window = $window_back_up
+  #
+  #   puts 'KKK' + $window.is_a?(Gosu::Window).to_s
+  #
+  #   Object.class_eval('Gosu::Image')
+  # end
 
-    @global_window_back_up = $window
-    $window = GosuMock::Window.new(200, 200, false)
-  end
-
-  config.after(:all) do
-    Gosu.send(:remove_const, :Window)
-    Gosu.send(:remove_const, :Image)
-    Gosu::Window = GosuWindowBackup
-    Gosu::Image = GosuImageBackup
-
-    $window = @global_window_back_up
-  end
-
-  config.after(:each) do
-    # clean up
-    $window.drawing_events.clear
-  end
+  # config.after(:each) do
+  #   # clean up
+  #   $window.drawing_events.clear
+  # end
 end
 
