ccc7391f0353a05a12da27a40f51f6d25bb22bf9
waynesayonara <waynesayonara@gmail.com>
2014-10-19 12:28:25 +0400

    code coverage set up. attempt.2 #1

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,9 +1,10 @@
-require 'codeclimate-test-reporter'
+if(ENV['CODECLIMATE_REPO_TOKEN'])
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 require_relative '../lib/imperium/imperium_main'
 require 'rspec'
-
-ENV['CODECLIMATE_REPO_TOKEN'] = '9d20f21fc1b16cadd10b7962abc77594fae7c6db52b86a1'
-CodeClimate::TestReporter.start
 
 $window = Imperium::ImperiumMain.new
 RSpec.configure do |config|
